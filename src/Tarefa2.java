3ab024dae95025afb720d327f959af5ecbdd8447
Anderson Andrade <139899277+AndersonCCandrade@users.noreply.github.com>
2024-01-27 10:54:23 -0300

    Tarefa 2 - Criando um programa java que apresente no terminal o texto “Seja bem vindo ao COMEX!“

---
+++ src/Tarefa2.java
@@ -0,0 +1,9 @@
+public class Tarefa2 {
+    public static void main(String[] args) {
+        //Tarefa 2 - Compile e execute o programa
+        //Crie um programa java que apresente no terminal o texto “Seja bem vindo ao COMEX!
+
+        System.out.println("Seja bem vindo ao COMEX!");
+        System.out.println();
+    }
+}
